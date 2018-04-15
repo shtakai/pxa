af9e9a76f3b1e52da4f38c057b5ea75f5ede7e03
Alyson T <shtakai@gmail.com>
2018-04-16 03:52:17 +0700

    Add fail test

---
+++ spec/feature/rspec_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'test', type: feature do
+  context 'test' do
+    it '1+1=2' do
+      expect(1+1).to eq(2)
+    end
+  end
+end

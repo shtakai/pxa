1b20bc61e6b60f5de2974a139f52610ca4a4edd3
Alyson T <shtakai@gmail.com>
2018-04-17 20:21:43 +0700

    Fix rubocop rule

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'home#index'
 
   get '/member_home', to: 'home#member', as: 'member_home'
 end
